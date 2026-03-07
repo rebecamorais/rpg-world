eefd0f9bf2f2655780c85684db030ecf8822dea6
Rebeca Morais <rebecamorais.12@gmail.com>
2026-03-07 17:07:50 -0300

    fix integration and unit test configs

--- test/vitest.config.integration.ts
+++ test/vitest.config.integration.ts
@@ -1,11 +1,15 @@
 import path from 'path';
+import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
+
+// 1. Importe o plugin
 
 import { setupTestEnvironment } from './setup-env';
 
 setupTestEnvironment();
 
 export default defineConfig({
+  plugins: [tsconfigPaths()],
   test: {
     root: path.resolve(__dirname, '../'),
     include: ['src/**/*.integration.test.ts'],
@@ -15,10 +19,5 @@ export default defineConfig({
       path.resolve(__dirname, './setup-vitest.ts'),
       path.resolve(__dirname, './setup-integration.ts'),
     ],
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, '../src'),
-    },
   },
 });
--- test/vitest.config.unit.ts
+++ test/vitest.config.unit.ts
@@ -1,7 +1,11 @@
 import path from 'path';
+import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 
+// 1. Importante para ler o tsconfig.json
+
 export default defineConfig({
+  plugins: [tsconfigPaths()],
   test: {
     root: path.resolve(__dirname, '../'),
     include: ['src/**/*.unit.test.ts'],
@@ -10,11 +14,6 @@ export default defineConfig({
     env: {
       NEXT_PUBLIC_SUPABASE_URL: 'http://mock-url.com',
       NEXT_PUBLIC_SUPABASE_ANON_KEY: 'mock-key',
-    },
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, '../src'),
     },
   },
 });
