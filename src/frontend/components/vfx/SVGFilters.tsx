c426e15335c17b3fdd986b10d014ecfe91987569
Rebeca Morais <rebecamorais.12@gmail.com>
2026-04-07 15:31:07 -0300

    feat: enhance sonic distortion VFX with new rumble animation and updated SVG filter parameters

--- src/frontend/components/vfx/SVGFilters.tsx
+++ src/frontend/components/vfx/SVGFilters.tsx
@@ -16,7 +16,7 @@ export default function SVGFilters() {
           <feTurbulence type="fractalNoise" baseFrequency="0.015" numOctaves="5" result="noise">
             <animate
               attributeName="baseFrequency"
-              dur="4s"
+              dur="1s"
               values="0.015;0.04;0.015"
               repeatCount="indefinite"
             />
@@ -24,29 +24,48 @@ export default function SVGFilters() {
           <feDisplacementMap
             in="SourceGraphic"
             in2="noise"
-            scale="50"
+            scale="40"
             xChannelSelector="R"
             yChannelSelector="G"
           />
         </filter>
 
-        {/* Thunder / Sonic Distortion (High frequency, lower scale) */}
-        <filter id="sonic-distortion">
-          <feTurbulence type="fractalNoise" baseFrequency="0.1" numOctaves="2" result="noise">
-            <animate
-              attributeName="baseFrequency"
-              dur="0.2s"
-              values="0.1;0.15;0.1"
-              repeatCount="indefinite"
-            />
+        {/* Thunder / Sonic Distortion (Sidechain Kick Drum) */}
+        <filter
+          id="sonic-distortion"
+          x="-20%"
+          y="-20%"
+          width="140%"
+          height="140%"
+          filterUnits="objectBoundingBox"
+          primitiveUnits="userSpaceOnUse"
+        >
+          <feTurbulence
+            type="fractalNoise"
+            baseFrequency="0.005 0.1"
+            numOctaves="4"
+            seed="0"
+            result="noise"
+          >
+            <animate attributeName="seed" from="0" to="100" dur="10s" repeatCount="indefinite" />
           </feTurbulence>
+          <feOffset dx="-50" dy="0" in="noise" result="offsetNoise">
+            <animate attributeName="dx" from="-200" to="200" dur="4s" repeatCount="indefinite" />
+          </feOffset>
           <feDisplacementMap
             in="SourceGraphic"
-            in2="noise"
-            scale="10"
+            in2="offsetNoise"
             xChannelSelector="R"
             yChannelSelector="G"
-          />
+          >
+            <animate
+              attributeName="scale"
+              dur="0.8s"
+              values="0; 100; 20; 0; 0"
+              keyTimes="0; 0.1; 0.15; 0.3; 1"
+              repeatCount="indefinite"
+            />
+          </feDisplacementMap>
         </filter>
       </defs>
     </svg>
